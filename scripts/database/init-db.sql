d3a1112d5e1e870ecbb1477864403762b99e5063
Usman Ikram <usman172@gmail.com>
2020-04-11 16:28:41 +0200

    Added testing database

--- scripts/database/init-db.sql
+++ scripts/database/init-db.sql
@@ -1,2 +1,4 @@
-# create testing database
+# create local database
 CREATE DATABASE IF NOT EXISTS `laravel-ci-training`;
+# create database for unit and accepotance test
+CREATE DATABASE IF NOT EXISTS `laravel-ci-training-testing`;
