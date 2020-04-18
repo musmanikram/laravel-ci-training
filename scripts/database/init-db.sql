5c90ee8e2fe8a18bc73701a2c013548900af8181
Usman Ikram <usman172@gmail.com>
2020-04-18 21:13:04 +0200

    application start and stop script + database script removed

--- scripts/database/init-db.sql
+++
@@ -1,4 +0,0 @@
-# create local database
-CREATE DATABASE IF NOT EXISTS `laravel-ci-training`;
-# create database for unit and accepotance test
-CREATE DATABASE IF NOT EXISTS `laravel-ci-training-testing`;
