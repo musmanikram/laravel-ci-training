5f295819cdecec179b0929463c3bab79daaa2548
Usman Ikram <usman172@gmail.com>
2020-04-26 15:03:09 +0200

    Getting tweets of user and his/her friends

--- app/User.php
+++ app/User.php
@@ -44,7 +44,15 @@ class User extends Authenticatable
 
     public function timeline()
     {
-        return Tweet::where('user_id', $this->id)->latest()->get();
+        $friendUserIds = $this->follows()->pluck('id');
+        return Tweet::whereIn('user_id', $friendUserIds)
+            ->orWhere('user_id', $this->id)
+            ->latest()->get();
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
     }
 
     public function follow(User $user)
