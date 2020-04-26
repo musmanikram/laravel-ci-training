415b842f455e7d5691f4060c69ece260677ccb99
Usman Ikram <usman172@gmail.com>
2020-04-26 19:20:47 +0200

    Added user profile

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -21,7 +21,7 @@ class TweetsController extends Controller
      */
     public function index()
     {
-        return view('home', [
+        return view('tweets.index', [
             'tweets' => auth()->user()->timeline()
         ]);
     }
--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     public function getAvatarAttribute()
     {
-        return 'https://i.pravatar.cc/40?u=' . $this->email;
+        return 'https://i.pravatar.cc/400?u=' . $this->email;
     }
 
     public function timeline()
@@ -63,5 +63,10 @@ class User extends Authenticatable
     public function follows()
     {
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'name';
     }
 }
