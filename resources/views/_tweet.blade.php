c20db551c7061f0a5371cd74413c9a3250a133c9
Usman Ikram <usman172@gmail.com>
2020-04-26 19:20:07 +0200

    Add some front end tweaks

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,17 +1,23 @@
 <div class="flex p-4 border-b border-b-gray-400" {{ testAttribute('tweet-id-'.$tweet->id) }}>
     <div class="mr-2 flex-shrink-0">
-        <img
-            src="{{ $tweet->user->avatar }}"
-            alt=""
-            class="rounded-full mr-2"
-            {{ testAttribute('tweet-user-img-'.$tweet->user->id) }}
-        >
+        <a href="{{ route('profile', $tweet->user->name) }}">
+            <img
+                src="{{ $tweet->user->avatar }}"
+                alt=""
+                class="rounded-full mr-2"
+                width="50"
+                height="50"
+                {{ testAttribute('tweet-user-img-'.$tweet->user->id) }}
+            >
+        </a>
     </div>
 
     <div>
-        <h5 class="font-bold mb-2" {{ testAttribute('tweet-user-name-'.$tweet->user->id) }}>
-            {{ $tweet->user->name }}
-        </h5>
+        <a href="{{ route('profile', $tweet->user->name) }}">
+            <h5 class="font-bold mb-2" {{ testAttribute('tweet-user-name-'.$tweet->user->id) }}>
+                {{ $tweet->user->name }}
+            </h5>
+        </a>
 
         <p class="text-sm" {{ testAttribute('tweet-body-'.$tweet->user->id) }}>
             {{ $tweet->body }}
