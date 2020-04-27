5a9eadd60c001bb9c17150821652a5863a7fbc74
Usman Ikram <usman172@gmail.com>
2020-04-27 17:54:43 +0200

    Made separate componennt for follow/unfollow

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -32,12 +32,7 @@
                 <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-sm mr-2">
                     Edit Profile
                 </a>
-                <form method="POST" action="/profiles/{{ $user->name }}/follow">
-                    @csrf
-                    <button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
-                        {{ auth()->user()->following($user) ? 'Unfollow me' : 'Follow me' }}
-                    </button>
-                </form>
+                <x-follow-button :user="$user"></x-follow-button>
 
             </div>
         </div>
