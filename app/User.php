fe0e2505adf8f79eafb338a462f6c5d5b71f3432
Usman Ikram <usman172@gmail.com>
2020-04-27 18:05:10 +0200

    Showing new tweet at top o profile page

--- app/User.php
+++ app/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
 
     public function tweets()
     {
-        return $this->hasMany(Tweet::class);
+        return $this->hasMany(Tweet::class)->latest();
     }
 
     public function profilePath()
