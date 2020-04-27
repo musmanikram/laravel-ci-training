9adbbbd3c675e6be598a0bcc6035edeb42eb701a
Usman Ikram <usman172@gmail.com>
2020-04-27 15:28:43 +0200

    Follow and edit profile buttons added

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -29,7 +29,14 @@
                     </a>
                 @endcan
 
-                {{--<x-follow-button :user="$user"></x-follow-button>--}}
+                <div>
+                    <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-sm mr-2">
+                        Edit Profile
+                    </a>
+                    <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
+                        Follow me
+                    </a>
+                </div>
             </div>
         </div>
 
