41d920c8e38325d2a95f219376c397dd8a7213ac
Usman Ikram <usman172@gmail.com>
2020-04-29 22:59:34 +0200

    Update user information added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -15,5 +16,18 @@ class ProfileController extends Controller
     public function edit(User $user)
     {
         return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+            'username' => ['required', 'string', 'max:255', Rule::unique('users')->ignore($user), 'alpha_dash'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user)],
+            'password' => ['required', 'string', 'min:8', 'max:255', 'confirmed'],
+        ]);
+
+        $user->update($attributes);
+        return redirect($user->path());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/tweets', 'TweetsController@store')->name('tweets');
     Route::post('/profiles/{user:username}/follow', 'FollowsController@store');
     Route::get('/profiles/{user:username}/edit', 'ProfileController@edit')->middleware('can:edit,user');
+    Route::patch('/profiles/{user:username}', 'ProfileController@update');
 });
 
 Route::get('/profiles/{user:username}', 'ProfileController@show')->name('profile');
