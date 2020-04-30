a15b671a20bfb3e178c9fe778973c4e5e2d713f1
Usman Ikram <usman172@gmail.com>
2020-04-30 19:09:28 +0200

    Fixed user profile link in side menu

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -25,7 +25,7 @@
         >Lists</a></li>
     <li><a
             class="font-bold text-lg mb-4 block"
-            href="{{ route('profile', auth()->user()->name) }}"
+            href="{{ route('profile', auth()->user()->username) }}"
         >Profile</a></li>
     <li><a
             class="font-bold text-lg block"
