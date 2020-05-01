0689aabfd83aa663965406269bf552b0de48c806
Usman Ikram <usman172@gmail.com>
2020-05-02 01:24:32 +0200

    Added avatar and unique username in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'username' => $faker->username,
+        'username' => $faker->unique()->username,
         'name' => $faker->name,
-        //'avatar' => $faker->image('avatars'),
+        'avatar' => $faker->image('avatars'),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
