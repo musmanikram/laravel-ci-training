e123fe154d000954a0d90e0fd9ee3ce68b215647
Usman Ikram <usman172@gmail.com>
2020-05-02 02:18:40 +0200

    Removed extra links

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -9,22 +9,6 @@
         >Explore</a></li>
     <li><a
             class="font-bold text-lg mb-4 block"
-            href="#"
-        >Notifications</a></li>
-    <li><a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >Messages</a></li>
-    <li><a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >Bookmarks</a></li>
-    <li><a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >Lists</a></li>
-    <li><a
-            class="font-bold text-lg mb-4 block"
             href="{{ route('profile', auth()->user()->username) }}"
         >Profile</a></li>
     <li>
